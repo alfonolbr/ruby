a97edf377d8edfbaa67f1c3ae61d74d5d765d44a
alfonolbr <63932282+alfonolbr@users.noreply.github.com>
2020-04-18 18:54:31 -0300

    Add files via upload

---
+++ classes.rb
@@ -0,0 +1,27 @@
+class Conta
+    attr_accessor :saldo, :nome
+    
+    def initialize(nome)
+        self.saldo = 0.0
+        self.nome = nome
+    end
+    
+    def deposita(valor)
+        # puts valor.class
+        self.saldo += valor
+        # puts 'Depositando a quantia de ' + valor.to_s + ' reais.'
+        puts "Depositando a quantia de #{valor} reais na conta de #{self.nome}."
+    end
+
+end
+
+c = Conta.new('Alexandre')
+
+# c.saldo = 0.0
+# puts c.saldo.class
+c.deposita(100.00)
+puts c.saldo
+
+c.deposita (10.00)
+puts c.saldo
+puts c.nome
